d80bd7d47a3f5c4adb566149b2397a80ee46b59b
gteijema <g.teijema@afterpay.nl>
2019-12-13 23:06:40 +0100

    add feature moving traffic  - traffic stop at red light  - moves when light becomes green

--- traffic-control.js
+++ traffic-control.js
@@ -1,93 +1,105 @@
 class TrafficController {
-	next_id = 0;
-	registered_lights = []; 
-	queue_sizes = [];
+    next_id = 0;
+    registered_lights = [];
+    queue_sizes = [];
+    frame_rate;
 
-	register(trafficLight) {
-		trafficLight.setId(this.next_id);
-		this.registered_lights.push(trafficLight);
-		this.queue_sizes.push(0);
-		this.determaineDependentTrafficLights(trafficLight);
-		this.next_id++;
+    constructor(frame_rate) {
+    	this.frame_rate = frame_rate;
 	}
 
-	determaineDependentTrafficLights(tl) {
-		if (tl.getId() > 0) {
-			tl.pushDependentOn(tl.getId() - 1);
-			tl.pushDependentOn(tl.getId() + 1);
-		}
-	}
+    register(trafficLight) {
+        trafficLight.setId(this.next_id);
+        this.registered_lights.push(trafficLight);
+        this.queue_sizes.push(0);
+        this.determaineDependentTrafficLights(trafficLight);
+        this.next_id++;
+    }
 
-	check() {
-		for (var i = 0; i < this.registered_lights.length; i++) {
-			let tl = this.registered_lights[i];
+    determaineDependentTrafficLights(tl) {
+        if (tl.getId() > 0) {
+            tl.pushDependentOn(tl.getId() - 1);
+            tl.pushDependentOn(tl.getId() + 1);
+        }
+    }
 
-			// actions
-			this.showQueueSize(tl);
-			this.setState(tl, this.determaineState(tl));
-		}
-	}
+    check() {
+        for (var i = 0; i < this.registered_lights.length; i++) {
+            let tl = this.registered_lights[i];
 
-	setState(tl, state) {
-		tl.setState(state);
-		tl.update();
-	}
+            // actions
+            this.showQueueSize(tl);
+            this.setState(tl, this.determaineState(tl));
+        }
+    }
 
-	determaineState(tl) {
-		// green, yellow, red
-		// state = [0x4, 0x2, 0x1];
+    setState(tl, state) {
+        tl.setState(state);
+        tl.update();
+    }
 
-		if (this.isDependentOnTrafficLights(tl)) {
-			// return red light when one of the dependent 
-			// traffic lights' state is green.
-			return 0x1;
-		}
+    determaineState(tl) {
+        // green, yellow, red
+        // state = [0x4, 0x2, 0x1];
 
-		let queueSize = this.getQueueSizeFor(tl);
-
-		if (queueSize > 0) {
-			this.setQueueSizeFor(tl);
-			return 0x4;
-		} else {
-			if (tl.getState() === 0x4) {
-				return 0x2;
-			}
+		if (frameCount % frame_rate !== 0) {
+			return tl.getState();
 		}
 
-		return 0x1;
-	}
+        if (this.isDependentOnTrafficLights(tl)) {
+            // return red light when one of the dependent
+            // traffic lights' state is green.
+            return 0x1;
+        }
 
-	isDependentOnTrafficLights(tl) {
-		let dep = tl.getDependentOn();
+        let queueSize = this.getQueueSizeFor(tl);
 
-		for (var i = 0; i < dep.length; i++) {
-			let dtl = this.registered_lights[dep[i]];
-			if (dtl && dtl.getState() === 0x4) {
-				return true;
-			}
-		}
-		return false;
-	}
+        if (queueSize > 0) {
+            this.setQueueSizeFor(tl, "-");
+            return 0x4;
+        } else {
+            if (tl.getState() === 0x4) {
+                return 0x2;
+            }
+        }
 
-	showQueueSize(tl) {
-		text(tl.getId() + ": " + this.queue_sizes[tl.getId()], 20, 15 + (20 * tl.getId()));
-	}
+        return 0x1;
+    }
 
-	getRegisteredLights() {
-		return "{" + this.next_id + "}";
-	}
+    isDependentOnTrafficLights(tl) {
+        let dep = tl.getDependentOn();
 
-	getQueueSizeFor(tl) {
-		return this.queue_sizes[tl.getId(0)];
-	}
+        for (var i = 0; i < dep.length; i++) {
+            let dtl = this.registered_lights[dep[i]];
+            if (dtl && dtl.getState() === 0x4) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	setQueueSizeFor(tl) {
-		return this.queue_sizes[tl.getId(0)]--;
-	}
+    showQueueSize(tl) {
+        text(tl.getId() + ": " + this.queue_sizes[tl.getId()], 20, 15 + (20 * tl.getId()));
+    }
 
-	setQueueSize() {
-		for (var i = 0; i < this.queue_sizes.length; i++) {		
-			this.queue_sizes[i] += Math.floor(Math.random() * 11);
- 		}
-	}
+    getRegisteredLights() {
+        return "{" + this.next_id + "}";
+    }
+
+    getQueueSizeFor(tl) {
+        return this.queue_sizes[tl.getId()];
+    }
+
+    setQueueSizeFor(tl, val) {
+        if (val === "+") {
+            this.queue_sizes[0] += 2;
+        }
+        this.queue_sizes[tl.getId()]--;
+    }
+
+    setQueueSize() {
+        for (var i = 0; i < this.queue_sizes.length; i++) {
+            this.queue_sizes[i] += Math.floor(Math.random() * 11);
+        }
+    }
 }
